e908600ea268dc83cb4382fb76659c386f13cb74
SuperSupreeth <supreeth.sandri@gmail.com>
2026-02-16 02:07:04 -0700

    synchronous reset done

--- input_buffer.sv
+++ input_buffer.sv
@@ -1,6 +1,37 @@
- module input_buffer(
-    input logic 
+ `include "GLOBAL_PARAMS.vh"
+ 
+ module input_buffer #(
+    parameter DATA_WIDTH = DATA_WIDTH,
+    parameter ADDR_WIDTH = ADDR_WIDTH
+ )(
+    input logic clk,
+    input logic rst_n,
+    input logic wr_en,
+    input logic rd_en,
+
+    
+    input logic [ ADDR_WIDTH - 1 : 0 ] wr_addr,
+    input logic [ ADDR_WIDTH - 1 : 0 ] rd_addr,
+
+
+    input  logic [ DATA_WIDTH - 1 : 0 ] wr_addr,
+
+    output logic [ DATA_WIDTH - 1 : 0 ] rd_addr,
+
  );
+
+
+    always_ff @( posedge clk or negedge rst_n ) begin : control_ff
+        
+        if(!rst_n) begin
+            rd_addr = '0;
+        end
+        else begin
+            
+        end
+
+
+    end
 	
  
  
